e5ba9f2b26b27cb2f3efd6de72aa1fc3c8bc2bbb
Neeraj Panwar <163735312+Neer-17@users.noreply.github.com>
2026-04-14 17:58:06 +0530

    Fixing Errors

--- app.py
+++ app.py
@@ -122,7 +122,8 @@ with right_cell3:
     print(len(data.index))
     if len(data.index) > 100000 :
         data = data.sample(n=100_000, random_state=42)
-    data_records = data[['lon', 'lat', 'mmsi_present', 'flag', 'geartype']].to_dict(orient='records')
+    data['color'] = data['loitering'].apply(color_picker)
+    data_records = data[['lon', 'lat', 'mmsi_present', 'flag', 'geartype','color']].to_dict(orient='records')
     deck = globe_plot(data_records)
     html = deck.to_html(as_string=True)
     components.html(html,height=600,scrolling=False)
